6868390780977550ad3a82cad8cfe228198445c2
Adam Borowski <adborowski@gmail.com>
2022-10-19 18:47:04 +0200

    Small cleanup of unused things

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { Link, useParams, useNavigate, useLocation } from "react-router-dom";
-import { Breadcrumb, Layout, Menu } from "antd";
 import { useState } from "react";
 import styles from "./App.module.css";
-import Footer from "./components/Footer/Footer";
 
 function App() {
-  let navigate = useNavigate();
-  let location = useLocation();
-  let params = useParams();
-
-  const [activeNavKey, setActiveNavKey] = useState("home");
-
   return (
     <div className={styles.home}>
       <h1>
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 import Projects from "./routes/projects";
 import About from "./routes/about";
 import Contact from "./routes/contact";
 import styles from "./App.module.css";
 import Footer from "./components/Footer/Footer";
-
 import Header from "./components/Header/Header";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
