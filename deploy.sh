e23db51f9249754f9c79b7150dce4514128069e4
Adam Borowski <adborowski@gmail.com>
2022-11-03 19:21:30 +0100

    Fixed URL issue with portrait

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 #! /bin/sh
 SWITCH="\033["
 NORMAL="${SWITCH}0m"
-GREEN="${SWITCH}1;32m"
+R1="${SWITCH}0;30m" #black
+R2="${SWITCH}1;31m" #red
+R3="${SWITCH}1;32m" #green
+R4="${SWITCH}1;33m" #yellow
+R5="${SWITCH}1;34m" #blue
+R6="${SWITCH}1;35m" #purple
+R7="${SWITCH}1;36m" #cyan
+R8="${SWITCH}1;37m" #white
 npm run build
 cd dist
 git add .
 git commit -m "Auto-deploy"
 git push
-echo "${GREEN}Deployed to production. Code is pulled every minute."
-echo "Available at: http://104.248.246.24/"
-echo "Also at: http://adamborowski.com${NORMAL}"
+echo ""
+echo "${R2}♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ${NORMAL}"
+echo "${R3}Deployed to production!"
+echo "${R4}104.248.246.24"
+echo "${R7}adamborowski.com"
+echo "${R2}♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ${NORMAL}"
+echo ""
 
