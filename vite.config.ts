827ff1ebd864909b4825bdd25a93634f87b6868b
Adam Borowski <adborowski@gmail.com>
2022-11-05 12:59:12 +0100

    Removed unused fonts. Changed html config in vite to control title and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import htmlPlugin from "vite-plugin-html-config";
+
+const htmlPluginOpt = {
+  title: "Adam Borowski",
+  favicon: "logo.svg",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlPlugin(htmlPluginOpt)],
   server: {
     port: 2137,
   },
